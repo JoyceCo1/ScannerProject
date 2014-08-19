b0fce8a5454d0749638e45309437cb2e79f11471
Connor.Joyce <joyceco1@mullenmail.com>
2014-08-19 13:12:38 -0600

    Initial Commit

---
+++ ScannerPractice/src/ScannerProject.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ScannerProject
+{	
+		  public static void main (String[] argus){
+		  for (int x = 500; x <= 997; x++ )
+		  {
+		    System.out.println(x+3);
+		  }
+		  
+		}}
